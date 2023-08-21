ddd693b18dbb761a74ba6e5560f61b25e89c94c0
Paulo César Moraes de Menezes <paulo.moraes@sou.unifal-mg.edu.br>
2023-08-21 07:29:13 -0300

    Arquivos estaticos

--- project/settings.py
+++ project/settings.py
@@ -119,7 +119,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# STATICFILE_DIRS = [
+#     BASE_DIR / 'base' / 'static',
+# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'base' / 'global' / 'static',
+    # BASE_DIR / 'base' / 'global' / 'static'
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
